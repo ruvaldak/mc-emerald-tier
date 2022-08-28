bc1855ac3942dfd9fead761a3b5db36ae5f4f046
Ruvaldak <truvaldak@gmail.com>
2022-08-28 16:57:53 -0400

    refactor

---
+++ src/main/java/com/ruvaldak/renewabletools/item/AmethystPickaxeItem.java
@@ -0,0 +1,10 @@
+package com.ruvaldak.renewabletools.item;
+
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class AmethystPickaxeItem extends PickaxeItem {
+    public AmethystPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
---
+++ src/main/java/com/ruvaldak/renewabletools/item/CopperHoeItem.java
@@ -0,0 +1,10 @@
+package com.ruvaldak.renewabletools.item;
+
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ToolMaterial;
+
+public class CopperHoeItem extends HoeItem {
+    public CopperHoeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
